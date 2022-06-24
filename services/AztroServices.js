6cf21261aa6121532836a3fe96b0d50d1b58ea80
Taisa Braga <taisabraga.adm@gmail.com>
2022-06-24 09:32:34 -0300

    Create API return

--- services/AztroServices.js
+++ services/AztroServices.js
@@ -1,17 +1,21 @@
 import axios from 'axios'
-import Vuetify from 'vuetify/lib'
 
-const URL = 'https://aztro.sameerkumar.website/?sign=aries&day=today'
-new Vuetify({
-  el: '#aztro',
-  data() {
-    return {
-      data: {}
-    }
-  },
-  created() {
-    axios.post(URL).then(response => {
-      this.data = response.data
+const URL = 'https://aztro.sameerkumar.website/'
+
+export function returnSigns() {
+  return axios.get(`${URL}/?sign=aries&day=today`).then(response => {
+    const Signs = response.data
+    return Signs?.map(sign => {
+      return {
+        daty: sign.current_date,
+        compatibility: sign.compatibility,
+        lucky_time: sign.lucky_time,
+        lucky_number: sign.lucky_number,
+        color: sign.color,
+        date_range: sign.date_range,
+        mood: sign.mood,
+        description: sign.description
+      }
     })
-  }
-})
+  })
+}
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -39,16 +39,29 @@
         </v-btn>
       </v-card-actions>
     </v-card>
+
+    <ul id="aztro">
+    <li>Current Date: {{data.current_date}}</li>
+    <li>Compatibility: {{data.compatibility}}</li>
+    <li>Lucky Number: {{data.lucky_number}}</li>
+    <li>Lucky Time: {{data.lucky_time}}</li>
+    <li>Color: {{data.color}}</li>
+    <li>Date Range: {{data.date_range}}</li>
+    <li>Mood: {{data.mood}}</li>
+    <li>Description: {{data.description}}</li>
+</ul>
   </div>
 </template>
 
 
 <script>
 
+
 export default {
   name: 'HomeView',
-
-
+  data(){
+    
+  }
 }
 
 
